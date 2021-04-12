b37d7b72858f6d0d2ee99c57d38b2ac8e642f754
Yolanda Brown <yolanbr5@yahoo.com>
2021-04-12 18:26:11 -0400

    This is my second update.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch('https://codice-boca.web.app/menu')
      .then(response => response.json())
      .then(menuList=> {
           console.log(menuList);
-          //array method
+          
          const menuCards =  menuList.map(entree => {
               return `<div class="menu-item">
               <h3 class="menu-item-title">${entree.title}</h3>
